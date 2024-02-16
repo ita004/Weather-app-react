4b8276f2e87fe1adba382605ccaea949b17ef1da
Shafi <shfahmd001@gamil.com>
2024-02-16 22:35:25 +0530

    first commit

---
+++ src/components/Api.jsx
@@ -0,0 +1,14 @@
+export const geoApiOptions ={
+   
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'eef02b3341mshe835cf1d0a651b5p1756f9jsn528db52e23e2',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+    };
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
+export const WEATHER_API_KEY = 'a30dd1b86eb63fdea3c0f9559c289a91';
+
+ 
